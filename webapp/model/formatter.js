70cfc026ddb691cdb7f885e9a45bc85ab836a8ab
i061957 <winters.rang@sap.com>
2022-09-20 09:30:16 +0800

    update

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,5 +1,7 @@
-sap.ui.define([], function () {
+sap.ui.define(["sap/ui/core/library"], function (coreLibrary) {
 	"use strict";
+
+	var ValueState = coreLibrary.ValueState;
 
 	return {
 
@@ -14,6 +16,15 @@ sap.ui.define([], function () {
 				return "";
 			}
 			return parseFloat(sValue).toFixed(2);
+		},
+		quantityState: function(iValue) {
+			if (iValue === 0) {
+				return ValueState.Error;
+			}else if (iValue <= 10) {
+				return ValueState.Warning;
+			}else {
+				return ValueState.Success; 
+			}
 		}
 
 	};
